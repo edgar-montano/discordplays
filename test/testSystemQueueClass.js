d91336fbc307921539d1fbcba8050b156d56fbef
Edgar Montano <em2532@nyu.edu>
2020-03-10 19:07:34 -0400

    added test for SystemQueue class

---
+++ test/testSystemQueueClass.js
@@ -0,0 +1,5 @@
+const inputs = require("../data/inputs.json");
+const SystemQueue = require("../systemqueue/SystemQueue");
+const testSystemQueueClass = () => {
+  let systemQueue = new systemQueue(inputs);
+};
